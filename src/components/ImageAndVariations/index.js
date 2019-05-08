8c530feaca14fa15ba2e29fc0340240ea4b4f40a
Donovan Hutchinson <donovanh@gmail.com>
2019-05-08 21:19:53 +0100

    Tidying up

--- src/components/ImageAndVariations/index.js
+++ src/components/ImageAndVariations/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { HideUntilLoaded } from 'react-animation'
 
 const renderPriceOption = ({ price, name }, setCurrentPrice, currentPrice) => (
   <li key={name}>
